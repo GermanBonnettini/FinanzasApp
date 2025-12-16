852b3a0918ab3f64a35a1da83be1d775676aa45e
German Bonnettini <geb.eihk@gmail.com>
2025-12-16 20:30:00 -0300

    Initial Commit

---
+++ FinanzasAppTests/FinanzasAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanzasAppTests.swift
+//  FinanzasAppTests
+//
+//  Created by H4MM3R-9 on 16/12/2025.
+//
+
+import Testing
+@testable import FinanzasApp
+
+struct FinanzasAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
